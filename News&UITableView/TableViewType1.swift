babcf72d41b26aeac44b969d1daa05a6501ca84b
Kwanghee-Kim <61907171+Kwanghee-Kim@users.noreply.github.com>
2021-05-16 23:44:34 +0900

    News&TableView 2단계

---
+++ News&UITableView/TableViewType1.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewType1.swift
+//  News&UITableView
+//
+//  Created by 김광희 on 2021/05/16.
+//
+
+import UIKit
+
+class TableViewType1: UITableViewCell {
+    
+    @IBOutlet weak var LabelText: UILabel!
+    
+    
+}
--- News&UITableView/ViewController.swift
+++ News&UITableView/ViewController.swift
@@ -23,16 +23,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // 데이터 종류
         // cell? -> table view cell
         // return 10 이므로 table view cell 10줄 반복
-        // 총 2가지 방법 중 하나 -> 임의의 셀 만들기
-        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "TableCellType1")
+        // 총 2가지 방법 1번 방법 -> 임의의 셀 만들기
+        // let cell = UITableViewCell.init(style: .default, reuseIdentifier: "TableCellType1")
+        // 2번 방법 -> 스토리보드 + identifier 를 이용한 방법
         
-        cell.textLabel?.text = "\(indexPath.row)"
+        let cell = TableViewMain.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! TableViewType1
+        cell.LabelText.text = "\(indexPath.row)"
+        
+        // as? as! -> 부모 자식 인스턴스 간 친자확인
+        // 텍스트를 넣기 위해 접근해야 하는 곳 => Type1 cell 내의 LabelText
+        // 접근 루트 => TableViewType1 (Type1 의 클래스)
+        // dequeueReusableCell 의 반환갑은 UITableViewCell 이고 TableViewType1 은 UITableViewCell 을 상속받았으나
+        // LabelText 는 UITableViewCell 가 아닌 TableViewType1 에 위치하고 있기 떄문에 as 를 통해
+        // 반환값을 UITableViewCell 에서 TableViewType1 로 바꿔줘야 함
         return cell
-        
-        
-        
+            
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You did select a row at \(indexPath.row)")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
